6998826436938f273d08ab022b57b80462326527
Siarhei.Kharytonchyk <Siarhei.Kharytonchyk@m2gen.com>
2024-06-26 11:28:22 +0200

    add test envs, change grouped transactions

--- server/routers/transactions.router.js
+++ server/routers/transactions.router.js
@@ -2,10 +2,30 @@ const Router = require('express');
 const router = Router();
 const transactionsController = require('../controllers/transactions.controller');
 const groupedTransactionsController = require('../controllers/grouped-transactions.controller');
+const transactionsByMonth = require('../controllers/year-grouped-transactions.controller');
+const CommonController = require('../controllers/common.controller');
+const envs = require('../config/envs');
 
 router.get('/transactions', transactionsController.getAllTransactions);
 router.get('/transactions-grouped', transactionsController.getTransactionsGroupByDate);
 router.get('/transactions-by-day', groupedTransactionsController.getTransactionsGroupedByDay);
+router.post('/transactions-by-month', (req, res) => {
+  const transactionsController = new CommonController(envs.transactions, 'TRANSACTIONS');
+  const categoriesController = new CommonController(envs.categories, 'CATEGORIES');
+  const cardsController = new CommonController(envs.cards, 'CARDS');
+
+  const transactions = transactionsController.readData();
+  const categories = categoriesController.readData();
+  const cards = cardsController.readData();
+
+  try {
+    const transactionsProcessed = transactionsByMonth.processTransactions(transactions, categories, cards);
+    res.send(transactionsProcessed);
+  } catch (e) {
+    console.error('Error occurred while processing transactions:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 router.post('/transactions-by-day', groupedTransactionsController.getTransactionsGroupedByDay);
 router.post('/add-transaction', transactionsController.addTransaction);
 router.post('/add-transaction-transfer', transactionsController.addTransactionTransfer);
